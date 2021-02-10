a613893786dcf7b1a89b36fc335c219958a779be
andrewg117 <andrewjgrant7@gmail.com>
2021-02-10 18:48:22 -0500

    Completed all tasks. Will work on markdown formatting

--- MarkdownPreviewer/index.js
+++ MarkdownPreviewer/index.js
@@ -1,13 +1,31 @@
 // The markdown text can't include more than 3 spaces before each new line
-const defualtText = `# Marked
-## Marked
-\`Marked\`
-**marked**
+const defualtText = `# This is a header H1 size
 
+## This is a header H2 size
 
+[This is a link](https://www.freecodecamp.org/)
+
+This is inline code \`let text = '';\`
+
+This is block code:
 \`\`\`javascript
-const text = '';
+let text = '';
+text += 'hello';
 \`\`\`
+
+1. First item
+   - First item
+2. Second item
+   - Second item
+3. Third item
+   - Third item
+
+**This is bold text**
+
+> This is a blockquote
+
+This is an image:
+![Trackstarz logo](https://yt3.ggpht.com/ytc/AAUvwngGAZPFBOkCbaUn23tK8y9Gs5wO16PHe8ugXBSheg=s900-c-k-c0x00ffffff-no-rj)
 `;
 
 
@@ -18,11 +36,12 @@ renderer.link = (href, title, text) => {
 
 marked.setOptions({
   renderer: renderer,
+  gfm: true,
   breaks: true,
-  highlight: (code, language) => {
-    const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
-    return hljs.highlight(validLanguage, code).value;
-  },
+  // highlight: (code, language) => {
+  //   const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+  //   return hljs.highlight(validLanguage, code).value;
+  // },
 });
 
 
@@ -52,7 +71,7 @@ class Marked extends React.Component {
   
   render() {
     return(
-      <div>
+      <div id="mark-container">
         <textarea id="editor" onChange={this.handleChange} value={this.state.text} />
         <div id="preview" dangerouslySetInnerHTML={this.getMarked()}></div>
       </div>
