ded6acfcee4889044419f3479353753ac561c81d
andrewg117 <andrewjgrant7@gmail.com>
2021-02-15 16:12:30 -0500

    Completed all tasks

--- Drum Machine/index.js
+++ Drum Machine/index.js
@@ -1,30 +1,66 @@
 const pads = [
   {
-    padText: "Q"
+    padText: "Q",
+    keyCode: 81,
+    audio:
+      "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-ClHat01.mp3",
+    audioText: "ClHat01"
   },
   {
-    padText: "W"
+    padText: "W",
+    keyCode: 87,
+    audio:
+      "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-ClHat02.mp3",
+    audioText: "ClHat02"
   },
   {
-    padText: "E"
+    padText: "E",
+    keyCode: 69,
+    audio:
+      "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-Kick01.mp3",
+    audioText: "Kick01"
   },
   {
-    padText: "A"
+    padText: "A",
+    keyCode: 65,
+    audio:
+      "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-Kick02.mp3",
+    audioText: "Kick02"
   },
   {
-    padText: "S"
+    padText: "S",
+    keyCode: 83,
+    audio:
+      "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-Kick03.mp3",
+    audioText: "Kick03"
   },
   {
-    padText: "D"
+    padText: "D",
+    keyCode: 68,
+    audio:
+      "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-OpHat.mp3",
+    audioText: "OpHat"
   },
   {
-    padText: "Z"
+    padText: "Z",
+    keyCode: 90,
+    audio:
+      "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-Shkr01.mp3",
+    audioText: "Shkr01"
   },
   {
-    padText: "X"
+    padText: "X",
+    keyCode: 88,
+    audio:
+      "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-Snr01.mp3",
+    audioText: "Snr01"
   },
   {
-    padText: "C"
+    padText: "C",
+    keyCode: 67,
+    audio:
+      "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-Tamb.mp3",
+    audioText: "Tamb"
   }
 ];
 
@@ -33,46 +69,106 @@ class DrumBox extends React.Component {
     super(props);
     this.handleClick = this.handleClick.bind(this);
     this.state = {
-      displayText: "Press a Pad"
+      displayText: "Press a Pad",
+      padGroup: []
     };
   }
 
   handleClick(value) {
-    console.log(value);
+    // console.log(value);
     this.setState({
       displayText: value
     });
   }
 
+  componentDidMount() {
+    this.setState({
+      padGroup: pads.map((pad, index) => (
+        <Pad
+          key={index}
+          handleClick={this.handleClick}
+          padText={pad.padText}
+          keyCode={pad.keyCode}
+          audio={pad.audio}
+          audioText={pad.audioText}
+        />
+      ))
+    });
+  }
+
   render() {
-    let padGroup = pads.map((pad, index) => (
-      <Pad
-        key={index}
-        handleClick={this.handleClick}
-        padText={pad.padText}
-      />
-    ));
     return (
       <div id="drum-container">
         <h1 id="display">{this.state.displayText}</h1>
-        <div id="drum-machine">
-          {padGroup}
-        </div>
+        <div id="drum-machine">{this.state.padGroup}</div>
       </div>
     );
   }
 }
 
-const Pad = (props) => {
+class Pad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKey = this.handleKey.bind(this);
+    this.state = {
+      isPressed: false
+    };
+  }
+
+  onPress() {
+    this.setState({
+      isPressed: !this.state.isPressed
+    });
+  }
+
+  playClip(id) {
+    let player = document.getElementById(id);
+    player.play();
+  }
+
   handleClick = (event) => {
-    props.handleClick(props.padText);
+    this.props.handleClick(this.props.audioText);
+    this.playClip(this.props.padText);
+    this.onPress();
+    setTimeout(() => this.onPress(), 100);
   };
-  return (
-    <div id="pad-container">
-      <button class="drum-pad" id={props.padText} onClick={handleClick}>{props.padText}</button>
-      <audio class="clip"  id={props.padText} />
-    </div>
-  );
-};
+
+  handleKey(event) {
+    if (event.keyCode == this.props.keyCode) {
+      this.props.handleClick(this.props.audioText);
+      this.playClip(this.props.padText);
+      this.onPress();
+      setTimeout(() => this.onPress(), 100);
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKey);
+
+    return () => {
+      window.removeEventListener("keydown", this.handleKey);
+    };
+  }
+
+  render() {
+    return (
+      <div id="pad-container">
+        <button
+          id={this.props.audioText}
+          className={!this.state.isPressed ? "drum-pad" : "drum-pad pressed"}
+          onClick={this.handleClick}
+        >
+          {this.props.padText}
+          <audio
+            className="clip"
+            id={this.props.padText}
+            src={this.props.audio}
+          />
+        </button>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<DrumBox />, document.getElementById("root"));
