d889a4df73e0dea5f3dfce7bca0388aac60a9663
andrewg117 <andrewjgrant7@gmail.com>
2021-02-17 16:42:00 -0500

    Completed css, cleaned up code, added comments

--- Drum Machine/index.js
+++ Drum Machine/index.js
@@ -1,17 +1,23 @@
+// Array of drum pads
+/*
+Audio ref: https://www.musicradar.com/news/drums/1000-free-drum-samples
+SampleRadar: 1,000 free drum samples
+by The MusicRadar team
+*/
 const pads = [
   {
     padText: "Q",
     keyCode: 81,
     audio:
       "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-ClHat01.mp3",
-    audioText: "ClHat01"
+    audioText: "Hat01"
   },
   {
     padText: "W",
     keyCode: 87,
     audio:
       "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-ClHat02.mp3",
-    audioText: "ClHat02"
+    audioText: "Hat02"
   },
   {
     padText: "E",
@@ -39,21 +45,21 @@ const pads = [
     keyCode: 68,
     audio:
       "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-OpHat.mp3",
-    audioText: "OpHat"
+    audioText: "Hat"
   },
   {
     padText: "Z",
     keyCode: 90,
     audio:
       "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-Shkr01.mp3",
-    audioText: "Shkr01"
+    audioText: "Shaker"
   },
   {
     padText: "X",
     keyCode: 88,
     audio:
       "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/Drum%20Machine/audio/CYCdh_VinylK1-Snr01.mp3",
-    audioText: "Snr01"
+    audioText: "Snare"
   },
   {
     padText: "C",
@@ -64,6 +70,7 @@ const pads = [
   }
 ];
 
+// Parent class that contains each pad and displays their audioText
 class DrumBox extends React.Component {
   constructor(props) {
     super(props);
@@ -74,8 +81,8 @@ class DrumBox extends React.Component {
     };
   }
 
+  // Displays audioText when a pad is pressed
   handleClick(value) {
-    // console.log(value);
     this.setState({
       displayText: value
     });
@@ -83,6 +90,7 @@ class DrumBox extends React.Component {
 
   componentDidMount() {
     this.setState({
+      // Creates each Pad component and sets them to the padGroup state
       padGroup: pads.map((pad, index) => (
         <Pad
           key={index}
@@ -97,15 +105,17 @@ class DrumBox extends React.Component {
   }
 
   render() {
+    const { displayText, padGroup } = this.state;
     return (
       <div id="drum-container">
-        <h1 id="display">{this.state.displayText}</h1>
-        <div id="drum-machine">{this.state.padGroup}</div>
+        <h1 id="display">{displayText}</h1>
+        <div id="drum-machine">{padGroup}</div>
       </div>
     );
   }
 }
 
+// Child class that renders each pad
 class Pad extends React.Component {
   constructor(props) {
     super(props);
@@ -116,34 +126,37 @@ class Pad extends React.Component {
     };
   }
 
+  // Toggles the css class when a pad is pressed
   onPress() {
     this.setState({
       isPressed: !this.state.isPressed
     });
   }
 
-  playClip(id) {
-    let player = document.getElementById(id);
+  // Takes in the pad and audio text to play the audio element and pass its text in to the display
+  playPad(padText, audioText) {
+    let player = document.getElementById(padText);
+    player.currentTime = 0;
     player.play();
+    this.props.handleClick(audioText); // Click function from the Parent class
+    this.onPress();
+    setTimeout(() => this.onPress(), 200);
   }
 
+  // Handles the mouse click
   handleClick = (event) => {
-    this.props.handleClick(this.props.audioText);
-    this.playClip(this.props.padText);
-    this.onPress();
-    setTimeout(() => this.onPress(), 100);
+    this.playPad(this.props.padText, this.props.audioText);
   };
 
+  // Handles the keyboard press
   handleKey(event) {
     if (event.keyCode == this.props.keyCode) {
-      this.props.handleClick(this.props.audioText);
-      this.playClip(this.props.padText);
-      this.onPress();
-      setTimeout(() => this.onPress(), 100);
+      this.playPad(this.props.padText, this.props.audioText);
     }
   }
 
   componentDidMount() {
+    // Listens for key presses
     window.addEventListener("keydown", this.handleKey);
 
     return () => {
@@ -152,19 +165,18 @@ class Pad extends React.Component {
   }
 
   render() {
+    const { isPressed } = this.state;
+    const { audioText, padText, audio } = this.props;
+
     return (
       <div id="pad-container">
         <button
-          id={this.props.audioText}
-          className={!this.state.isPressed ? "drum-pad" : "drum-pad pressed"}
+          id={audioText}
+          className={!isPressed ? "drum-pad" : "drum-pad pressed"}
           onClick={this.handleClick}
         >
-          {this.props.padText}
-          <audio
-            className="clip"
-            id={this.props.padText}
-            src={this.props.audio}
-          />
+          {padText}
+          <audio className="clip" id={padText} src={audio} />
         </button>
       </div>
     );
