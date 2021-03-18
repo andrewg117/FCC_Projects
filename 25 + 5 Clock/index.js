66a7bc168151eee01819802634961a15e1d882a5
andrewg117 <andrewjgrant7@gmail.com>
2021-03-18 14:47:31 -0400

    completed user stories, css, and added comments

--- 25 + 5 Clock/index.js
+++ 25 + 5 Clock/index.js
@@ -1,3 +1,11 @@
+/* 
+  https://orangefreesounds.com/cool-alarm-tone-notification-sound/
+  Cool Alarm Tone Notification Sound
+  Posted by: by alexander June 19, 2018
+  The sound effect is permitted for non-commercial use under license “Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)”
+*/
+const audio =
+  "https://raw.githubusercontent.com/andrewg117/FCC_Projects/main/25 %2B 5 Clock/audio/Cool-alarm-tone-notification-sound.mp3";
 
 class Clock extends React.Component {
   constructor(props) {
@@ -5,15 +13,18 @@ class Clock extends React.Component {
     this.startClock = this.startClock.bind(this);
     this.resetClock = this.resetClock.bind(this);
     this.incTime = this.incTime.bind(this);
+    this.alarm = 0; 
+
     this.state = {
-      timeLeft: 1500,
-      breakLength: 5,
-      sessionLength: 25,
-      isStart: false,
-      onBreak: false
+      timeLeft: 1500, // value of current time to be displayed in the time-left element
+      breakLength: 5, // value of current break time to be displayed in the break-length element
+      sessionLength: 25, // value of current session time to be displayed in the session-length element
+      isStart: false, // bool to change the Start/Pause state
+      onBreak: false // bool to change the session/break state
     };
   }
 
+  // converts the time lengths to mm:ss
   convertTime(time) {
     let length = time;
     let sec = length % 60;
@@ -23,41 +34,62 @@ class Clock extends React.Component {
       min.toString().padStart(2, "0") + ":" + sec.toString().padStart(2, "0")
     );
   }
-  
-  changeSession() {
-    if(!this.state.onBreak && this.state.timeLeft == 0){
-      this.setState({
-        onBreak: true,
-        timeLeft: (this.state.breakLength + 1) * 60
-      });
-    } else if(this.state.onBreak && this.state.timeLeft == 0){
-      this.setState({
-        onBreak: false,
-        timeLeft: (this.state.sessionLength + 1) * 60
-      });
-    } 
+
+  // plays the alarm object and pauses it after 3 seconds
+  playAlarm() {
+    this.alarm.currentTime = 0;
+    this.alarm.play();
+    setTimeout(() => {
+      this.alarm.pause();
+    }, 3000);
   }
 
+  // alternates the timeLeft state based on the onBreak bool and plays the alarm
+  changeSession() {
+    const { sessionLength, breakLength, onBreak, timeLeft } = this.state;
+
+    if (!onBreak && timeLeft == 0) {
+      this.playAlarm();
+
+      this.setState({
+        onBreak: true,
+        timeLeft: breakLength * 60 + 1
+      });
+    } else if (onBreak && timeLeft == 0) {
+      this.playAlarm();
+
+      this.setState({
+        onBreak: false,
+        timeLeft: sessionLength * 60 + 1
+      });
+    }
+  }
+
+  // alternates the isStart state to start/pause the clock when the start_stop element is pressed
   startClock() {
-     let timer = setInterval(() => {
+    // a timer that decrements the timeLeft state and calls the changeSession function when timeLeft is 00:00
+    let timer = setInterval(() => {
       this.changeSession();
-      if(this.state.isStart && this.state.timeLeft > 0){
+
+      if (this.state.isStart && this.state.timeLeft > 0) {
         this.setState({
           timeLeft: this.state.timeLeft - 1
         });
       } else {
         clearInterval(timer);
-        // console.log(timer);
       }
     }, 1000);
-    
+
     this.setState({
-      isStart: !this.state.isStart,
+      isStart: !this.state.isStart
     });
   }
 
+  // resets the clock state and alarm to default when the reset element is pressed
   resetClock() {
-    clearInterval(this.state.timer);
+    this.alarm.pause();
+    this.alarm.currentTime = 0;
+
     this.setState({
       timeLeft: 1500,
       breakLength: 5,
@@ -66,27 +98,22 @@ class Clock extends React.Component {
       onBreak: false
     });
   }
-  
+
+  // increases/decreases the session/break length state based on the elements id
   incTime(e) {
     const { sessionLength, breakLength, onBreak, timeLeft } = this.state;
-    
+
     if (e.target.id == "session-increment" && sessionLength < 60) {
       this.setState({
         sessionLength: sessionLength + 1,
-        timeLeft: !onBreak ? (sessionLength + 1) * 60: timeLeft
+        timeLeft: !onBreak ? (sessionLength + 1) * 60 : timeLeft
       });
-    } else if (
-      e.target.id == "session-decrement" &&
-      sessionLength > 1
-    ) {
+    } else if (e.target.id == "session-decrement" && sessionLength > 1) {
       this.setState({
         sessionLength: sessionLength - 1,
-        timeLeft: !onBreak ? (sessionLength - 1) * 60: timeLeft
+        timeLeft: !onBreak ? (sessionLength - 1) * 60 : timeLeft
       });
-    } else if (
-      e.target.id == "break-increment" &&
-      breakLength < 60 
-    ) {
+    } else if (e.target.id == "break-increment" && breakLength < 60) {
       this.setState({
         breakLength: breakLength + 1,
         timeLeft: onBreak ? (breakLength + 1) * 60 : timeLeft
@@ -98,9 +125,11 @@ class Clock extends React.Component {
       });
     }
   }
-  
-  
+
   componentDidMount() {
+    // initializes the alarm object to the beep element and sets the volumn to 20%
+    this.alarm = document.getElementById("beep");
+    this.alarm.volume = 0.2;
   }
 
   render() {
@@ -116,27 +145,34 @@ class Clock extends React.Component {
     return (
       <div id="clockFrame">
         <section style={{ "grid-area": "session" }}>
-          <h2 id="timer-label">{onBreak ? 'Break' : 'Session'}</h2>
-          <h3 id="time-left">{this.convertTime(timeLeft)}</h3>
+          <h2 id="timer-label">{onBreak ? "Break" : "Session"}</h2>
+          <h1 id="time-left">{this.convertTime(timeLeft)}</h1>
+          <audio id="beep" src={audio} />
         </section>
         <section style={{ "grid-area": "brLen" }}>
-          <label id="break-label">Break Length</label>
-          <p id="break-length">{breakLength}</p>
-          <button id="break-increment" onClick={!isStart? this.incTime: null}>
-            Inc
+          <label id="break-label">Break Length</label> <br/>
+          <button id="break-increment" onClick={!isStart ? this.incTime : null}>
+            +
           </button>
-          <button id="break-decrement" onClick={!isStart? this.incTime: null}>
-            Dec
+          <span id="break-length">{breakLength}</span>
+          <button id="break-decrement" onClick={!isStart ? this.incTime : null}>
+            -
           </button>
         </section>
         <section style={{ "grid-area": "sessLen" }}>
-          <label id="session-label">Session Length</label>
-          <p id="session-length">{sessionLength}</p>
-          <button id="session-increment" onClick={!isStart? this.incTime: null}>
-            Inc
+          <label id="session-label">Session Length</label><br/>
+          <button
+            id="session-increment"
+            onClick={!isStart ? this.incTime : null}
+          >
+            +
           </button>
-          <button id="session-decrement" onClick={!isStart? this.incTime: null}>
-            Dec
+          <span id="session-length">{sessionLength}</span>
+          <button
+            id="session-decrement"
+            onClick={!isStart ? this.incTime : null}
+          >
+            -
           </button>
         </section>
         <section style={{ "grid-area": "control" }}>
--- 25 + 5 Clock/readMe.txt
+++ 25 + 5 Clock/readMe.txt
@@ -51,8 +51,8 @@ COMPLETE: User Story #24: When a break countdown reaches zero (NOTE: timer MUST 
 
 COMPLETE: User Story #25: When a break countdown reaches zero (NOTE: timer MUST reach 00:00), a new session countdown should begin, counting down from the value currently displayed in the id="session-length" element.
 
-TODO: User Story #26: When a countdown reaches zero (NOTE: timer MUST reach 00:00), a sound indicating that time is up should play. This should utilize an HTML5 audio tag and have a corresponding id="beep".
+COMPLETE: User Story #26: When a countdown reaches zero (NOTE: timer MUST reach 00:00), a sound indicating that time is up should play. This should utilize an HTML5 audio tag and have a corresponding id="beep".
 
-TODO: User Story #27: The audio element with id="beep" must be 1 second or longer.
+COMPLETE: User Story #27: The audio element with id="beep" must be 1 second or longer.
 
-TODO: User Story #28: The audio element with id of beep must stop playing and be rewound to the beginning when the element with the id of reset is clicked.
+COMPLETE: User Story #28: The audio element with id of beep must stop playing and be rewound to the beginning when the element with the id of reset is clicked.
